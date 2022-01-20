5c89ef63725537b42041ac99a8f13301316942b3
brandleyzhou <brandleyzhou@gmail.com>
2022-01-20 12:35:09 +0000

    retrain low res model

--- start2train.sh
+++ start2train.sh
@@ -1,3 +1,1 @@
-#python train.py --use_stereo --height 320 --width 1024 --scheduler_step_size 14  --batch_size 4  --model_name mono_model --png --data_path data_path/kitti
-#python train.py --height 320 --width 1024 --scheduler_step_size 14  --batch_size 8  --model_name mono_model --png --data_path data_path/kitti
-python train.py --scheduler_step_size 14  --batch_size 12  --model_name mono_model --png --data_path data_path/kitti
+python train.py --scheduler_step_size 14  --batch_size 8  --model_name mono_model --png --data_path data_path/kitti
